2e3dceed051d365c402c90ad5b8e589834ed7561
wann31828 <s950659@gmail.com>
2019-03-19 17:27:31 +0800

    for JP speech
    support jsut_ver1.1 dataset

--- hparams.py
+++ hparams.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 hparams = tf.contrib.training.HParams(
 	# Comma-separated list of cleaners to run on text prior to training and eval. For non-English
 	# text, you may want to use "basic_cleaners" or "transliteration_cleaners".
-	cleaners='english_cleaners',
+	cleaners='basic_cleaners',
 
 
 	#If you only have 1 GPU or want to use only one GPU, please set num_gpus=0 and specify the GPU idx on run. example:
@@ -107,7 +107,7 @@ hparams = tf.contrib.training.HParams(
 	#Limits
 	min_level_db = -100,
 	ref_level_db = 20,
-	fmin = 55, #Set this to 55 if your speaker is male! if female, 95 should help taking off noise. (To test depending on dataset. Pitch info: male~[65, 260], female~[100, 525])
+	fmin = 95, #Set this to 55 if your speaker is male! if female, 95 should help taking off noise. (To test depending on dataset. Pitch info: male~[65, 260], female~[100, 525])
 	fmax = 7600, #To be increased/reduced depending on data.
 
 	#Griffin Lim
@@ -118,7 +118,7 @@ hparams = tf.contrib.training.HParams(
 
 	#Tacotron
 	#Model general type
-	outputs_per_step = 1, #number of frames to generate at each decoding step (increase to speed up computation and allows for higher batch size, decreases G&L audio quality)
+	outputs_per_step = 2, #number of frames to generate at each decoding step (increase to speed up computation and allows for higher batch size, decreases G&L audio quality)
 	stop_at_any = True, #Determines whether the decoder should stop when predicting <stop> to any frame or to all of them (True works pretty well)
 	batch_norm_position = 'after', #Can be in ('before', 'after'). Determines whether we use batch norm before or after the activation function (relu). Matter for debate.
 	clip_outputs = True, #Whether to clip spectrograms to T2_output_range (even in loss computation). ie: Don't penalize model for exceeding output range and bring back to borders.
@@ -341,29 +341,45 @@ hparams = tf.contrib.training.HParams(
 	#Eval sentences (if no eval text file was specified during synthesis, these sentences are used for eval)
 	sentences = [
 	# From July 8, 2017 New York Times:
-	'Scientists at the CERN laboratory say they have discovered a new particle.',
-	'There\'s a way to measure the acute emotional intelligence that has never gone out of style.',
-	'President Trump met with other leaders at the Group of 20 conference.',
-	'The Senate\'s bill to repeal and replace the Affordable Care Act is now imperiled.',
+	#'Scientists at the CERN laboratory say they have discovered a new particle.',
+	#'There\'s a way to measure the acute emotional intelligence that has never gone out of style.',
+	#'President Trump met with other leaders at the Group of 20 conference.',
+	#'The Senate\'s bill to repeal and replace the Affordable Care Act is now imperiled.',
 	# From Google's Tacotron example page:
-	'Generative adversarial network or variational auto-encoder.',
-	'Basilar membrane and otolaryngology are not auto-correlations.',
-	'He has read the whole thing.',
-	'He reads books.',
-	'He thought it was time to present the present.',
-	'Thisss isrealy awhsome.',
-	'The big brown fox jumps over the lazy dog.',
-	'Did the big brown fox jump over the lazy dog?',
-	"Peter Piper picked a peck of pickled peppers. How many pickled peppers did Peter Piper pick?",
-	"She sells sea-shells on the sea-shore. The shells she sells are sea-shells I'm sure.",
-	"Tajima Airport serves Toyooka.",
+	#'Generative adversarial network or variational auto-encoder.',
+	#'Basilar membrane and otolaryngology are not auto-correlations.',
+	#'He has read the whole thing.',
+	#'He reads books.',
+	#'He thought it was time to present the present.',
+	#'Thisss isrealy awhsome.',
+	#'The big brown fox jumps over the lazy dog.',
+	#'Did the big brown fox jump over the lazy dog?',
+	#"Peter Piper picked a peck of pickled peppers. How many pickled peppers did Peter Piper pick?",
+	#"She sells sea-shells on the sea-shore. The shells she sells are sea-shells I'm sure.",
+	#"Tajima Airport serves Toyooka.",
 	#From The web (random long utterance)
 	# 'On offering to help the blind man, the man who then stole his car, had not, at that precise moment, had any evil intention, quite the contrary, \
 	# what he did was nothing more than obey those feelings of generosity and altruism which, as everyone knows, \
 	# are the two best traits of human nature and to be found in much more hardened criminals than this one, a simple car-thief without any hope of advancing in his profession, \
 	# exploited by the real owners of this enterprise, for it is they who take advantage of the needs of the poor.',
 	# A final Thank you note!
-	'Thank you so much for your support!',
+	#'Thank you so much for your support!',
+	'Handan suru Baai ga aru no de aru.',
+	'Roku Byou, Shichi Byou, Hachi Byou, Kyuu Byou, Juu Byou, Nani Byou',
+	'Itai Shori no Jisshi ha, Koushuu Eisei wo Iji shi',
+	'Joushiki Teki na Kankaku ni Somuku Henshuu ga are ba, ironna Nin ga, sono Henshuu wo Shuusei shi tari.',
+	'Shi Hyou, Go Hyou, Roku Hyou, Shichi Hyou, Hachi Hyou, Kyuu Hyou, Juu Hyou, Nani Hyou.',
+	'benchi puresu Kyougi ni ha Genmitsu ni Sadame rare ta ru-ru ga ari.',
+	'kore mo Jinkou Teki ni Fuyasa re ta TouHebi no Tokusei da ga.',
+	'Hisshi no Omoi de eku tachi Kazu Atama no Ridatsu ni Seikou suru ga.',
+	'aru Yoru Ippanjin he no Boukou Jiken wo Okoshi.',
+	'akkenaku Jomei Shobun sa re te shimau.',
+	'Riyou Mono ga Shiro no Shashin wo Toukou shi tari.',
+	'Joukaku Suki he Muke ta komyunitei Kinou mo Youi sa re te iru.',
+	'Tei ha AreHate te Meshitsukai tachi mo Sari.',
+	'MatsuTekiHana ha Ouji nai.',
+	'furesuko Ga You no Jitsubutsu Dai no Shitae wo Tsukutsu ta.',
+	'sarani vaza-ri ha, NenOi ta sodoma ha sarani Namakeru you ni nari.',
 	],
 
 	#Wavenet Debug
